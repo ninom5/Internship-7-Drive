5106fc3c2744a58c71178dffcdb5ddbd460ba7d5
ninom5 <nmilas00@fesb.hr>
2024-12-19 10:20:16 +0100

    fixed all errors; successfull registering users and adding them to db

--- Drive/Drive.Presentation/Program.cs
+++ Drive/Drive.Presentation/Program.cs
@@ -1,17 +1,27 @@
 ﻿using Drive.Presentation.Interfaces;
 using Drive.Presentation.Factories;
+using Drive.Data.Entities;
+using Drive.Domain.Services;
+using Microsoft.EntityFrameworkCore;
 public class Program
 {
     public static void Main()
     {
+        var options = new DbContextOptionsBuilder<DriveDbContext>()
+           .UseNpgsql("Host=127.0.0.1;Port=5432;Database=Drive;User Id=postgres;Password=rootuser")
+           .Options;
+        var dbContext = new DriveDbContext(options);
+
+        var userService = new UserService(dbContext);
+
+        MenuFactory.Initialize(userService);
+
         IMenu mainMenu = MenuFactory.CreateMenu("MainMenu");
 
-        while(true)
+        while (true)
         {
             mainMenu.Display();
             mainMenu.HandleInput();
         }
     }
 }
-
-//var userService = new UserService(dbContext);
